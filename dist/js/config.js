3201a348c8df1619c2f8ff432d0c1b893c0914a7
SlavkoSipka <bogdangradjanin@gmail.com>
2026-03-25 21:41:31 +0100

    kontatk

--- dist/js/config.js
+++ dist/js/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     // EmailJS Configuration
     // Dobićeš ove vrednosti nakon registracije na https://www.emailjs.com/
     EMAILJS_PUBLIC_KEY: 'smz_B_DFSpZ4OLD2C',  // Idi na: Account > API Keys > Public Key
-    EMAILJS_SERVICE_ID: 'service_hk6x4d9',          // Idi na: Email Services > Add Service
+    EMAILJS_SERVICE_ID: 'service_i1e680q',          // Email Services (EmailJS dashboard)
     EMAILJS_TEMPLATE_ID: 'template_j94voyd'         // Idi na: Email Templates > Create Template
 };
     
